b60a5894e8800f26ca9a046917046d4ff677cd02
imAnujMishra <anujvpmishra@gmail.com>
2024-07-11 13:52:43 +0530

    Added All files for the Project

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  fs.readdir("./files", function (err, files) {
+    res.render("index", { files: files });
+  });
+});
+app.post("/create", function (req, res) {
+  fs.writeFile(
+    `./files/${req.body["task-title"].split(" ").join("")}.txt`,
+    req.body["task-text"],
+    function (err) {
+      res.redirect("/");
+    }
+  );
+  
+});
+app.get("/file/:filename", function (req, res) {
+  const name = req.params.filename;
+  fs.readFile(`./files/${name}`, "utf-8", function (err, filedata) {
+    res.render("show", { filename: name, filedata: filedata });
+  });
+  
+});
+app.get("/edit/:filename", function (req, res) {
+  res.render("edit", { filename: req.params.filename });
+});
+app.post("/edit", function (req, res) {
+  fs.rename(
+    `./files/${req.body.previous}`,
+    `./files/${req.body.new}`,
+    function (err) {
+      res.redirect("/");
+    }
+  )});
+
+app.listen(3000);
